e143ec4cca7dd32adfde5d1304cd7ff362aaef64
dhavalbheda <dhavalbhedadb@gmail.com>
2021-03-01 19:11:14 +0530

    First Commit

---
+++ First Project/Assets/scripts/TriggerEvents.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TriggerEvents : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("player"))
+        {
+         //   Debug.Log("call");
+        }
+        Debug.Log(other.gameObject.tag);
+       // other.gameObject.SetActive(false);
+    }
+/*
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            Debug.Log("Object In");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            Debug.Log("Object Exit");
+        }
+    }*/
+}
